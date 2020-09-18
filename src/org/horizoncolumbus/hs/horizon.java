15aa235a6be431b3e29b60f56be9875937d071ed
HSA-Student <student.e.amofa@horiozncolumbus.org>
2020-09-18 12:22:07 -0400

    Homework file uploaded here.

---
+++ src/org/horizoncolumbus/hs/horizon.java
@@ -0,0 +1,10 @@
+package org.horizoncolumbus.hs;
+
+public class horizon {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Good " + args[0] + ", " + args[1] + "!");
+
+    }
+}
